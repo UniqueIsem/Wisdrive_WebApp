2949df1608caa0372ceae15cc523e208c9660923
Isaac Santos <isaac.emanuel@live.com>
2025-02-18 00:32:33 -0600

    feat(*) upload project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { supabase } from "./supabase/client";
+import { useEffect } from "react";
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import NotFound from './pages/NotFound'
+//import { TaskContextProvider } from "./context/TaskContext";
+//import { AuthProvider } from "./context/AuthContext";
 
 function App() {
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((_event, session) => {
+      if (!session) {
+        navigate("/login");
+      } else {
+        navigate("/");
+      }
+    });
+  }, [navigate]);
 
   return (
     <>
-      
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </>
+
   )
 }
 
